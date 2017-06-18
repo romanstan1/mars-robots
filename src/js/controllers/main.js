87dd142f19ad247419751d58f871182e8b0baff7
Roman Stankiewicz <roman.stan3@gmail.com>
2017-06-18 19:30:12 +0100

    linked inputs, ngmodels and controllers

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -9,9 +9,21 @@ function MainCtrl() {
   vm.height = 30;
   vm.width = 50;
 
-
   vm.rows = new Array(vm.height);
   vm.columns = new Array(vm.width);
+  vm.activeRobot = null;
+  vm.output = null;
 
 
+  function createRobot() {
+    console.log(vm.activeRobot);
+  }
+
+  vm.createRobot = createRobot;
+
+
+  function moveRobot() {
+    console.log(vm.output);
+  }
+  vm.moveRobot = moveRobot;
 }
