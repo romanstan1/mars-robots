252ea58dff8b4c1f43d41da1bbca5d0b4c02a575
Roman Stankiewicz <roman.stan3@gmail.com>
2017-06-18 21:56:53 +0100

    robot can be created but not moved

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -14,9 +14,31 @@ function MainCtrl() {
   vm.activeRobot = null;
   vm.output = null;
 
+  vm.direction = null;
+
 
   function createRobot() {
-    console.log(vm.activeRobot);
+    // let activeRobot = document.getElementsByClassName(`cell x${xCoordinate} y${yCoordinate}`);
+    // activeRobot[0].addClass('activeRobot');
+    // activeRobot.addClass('activeRobot');
+    // console.log(activeRobot);
+
+    const input = vm.activeRobot.split('-');
+
+    const xCord = parseInt(input[0]) + 10;
+    const yCord = parseInt(input[1]) + 10;
+
+    console.log(xCord);
+    console.log(yCord);
+    
+    vm.direction = input[2];
+
+    const activeRobot = angular.element( document.querySelector( `#x${xCord}y${yCord}` ));
+    activeRobot.addClass('activeRobot');
+
+    console.log(activeRobot);
+
+    //angular.element('li.cell x0 y0').addClass("activeRobot");
   }
 
   vm.createRobot = createRobot;
